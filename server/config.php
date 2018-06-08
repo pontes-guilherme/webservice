d7e737361bc374f3b125b40914263a48e8f6e73f
Guilherme Pontes <gui97p.p@gmail.com>
2018-06-08 15:15:43 -0300

    Arquivos de registro de compras

--- server/config.php
+++ server/config.php
@@ -1,5 +1,6 @@
 <?php
 
-define('PASSAGENS_FILE',        'data/passagens.json');
-define('HOSPEDAGENS_FILE',      'data/hospedagens.json');
-define('COMPRA_PASSAGENS_FILE', 'data/compra_passagens.json');
+define('PASSAGENS_FILE',            'data/passagens.json');
+define('HOSPEDAGENS_FILE',          'data/hospedagens.json');
+define('COMPRA_PASSAGENS_FILE',     'data/compra_passagens.json');
+define('COMPRA_HOSPEDAGENS_FILE',   'data/compra_hospedagens.json');
